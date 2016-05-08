52cd1250e1495c197c91ecbc107eee5b1b56fe93
dengshilong <dengshilong1988@gmail.com>
2016-05-08 10:46:27 +0800

    add HeadTail solution description

--- src/chapter1/HeadTail.java
+++ src/chapter1/HeadTail.java
@@ -5,6 +5,14 @@ package chapter1;
  * author: robinjia
  * email: dengshilong1988@gmail.com
  */
+
+/*
+可以用变量prefix来表示前置和，用suffix来表示后置和，
+用i表示前置和累加元素的位置，i从前往后加，用j表示后置和累加元素的位置, j从后往前加。
+当prefix > suffix时，累加后置和，也就是j向前走；
+当prefix < suffix时，累加前置和，也就是i往后走；
+当prefix == suffix时，同时累加前置和与后置和，也就是i往后走，j往前走
+ */
 public class HeadTail {
     public static int headTail(int[] nums) {
         int i = 0;
