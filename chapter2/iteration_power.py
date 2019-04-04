c73f33247337f5b107c7ed300b3d2df4b666bc39
dengshilong <dengshilong1988@gmail.com>
2019-04-04 13:15:02 +0800

    fix: 增加数值自乘非递归解第二种解法

--- chapter2/iteration_power.py
+++ chapter2/iteration_power.py
@@ -10,9 +10,21 @@ def iteration_power(m, n):
         m = m * m
     return p
 
+def iteration_power_two(m, n):
+    p = 1
+    while n >= 1:
+        if n & 1:
+            p *= m
+            n -= 1
+        else:
+            m = m * m
+            n = n >> 1
+    return p
 
 if __name__ == "__main__":
     for i in range(1, 10):
         for j in range(1, 10):
             assert iteration_power(i, j) == i ** j
+            assert iteration_power_two(i, j) == i ** j
     print(iteration_power(3, 4))
+    print(iteration_power_two(3, 4))
