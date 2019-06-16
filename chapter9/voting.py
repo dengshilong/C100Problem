5bda18fac0564d81e44317922df5469a6f97940f
dengshilong <dengshilong1988@gmail.com>
2019-06-16 15:18:16 +0800

    docs: 增加投票问题

---
+++ chapter9/voting.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+
+def voting(a):
+    m = 0
+    count = 0
+    for i in range(len(a)):
+        if count == 0:
+            m = a[i]
+            count += 1
+        else:
+            if m == a[i]:
+                count += 1
+            else:
+                count -= 1
+    if count == 0:
+        return 0
+    count = a.count(m)
+    return m if int(count * 1.0 / len(a) + 0.5) else 0
+
+
+if __name__ == "__main__":
+    assert voting([1, 8, 1, 100, 1]) == 1
+    assert voting([1, 2, 1, 2, 3]) == 0
